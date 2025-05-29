f33153214a132009ab01493276f3865999ae9c9c
Niakrist <ak_88dron@inbox.ru>
2025-05-29 11:32:53 +0300

    feat: emplement bearingController

--- app/controller/bearingController.js
+++ app/controller/bearingController.js
@@ -1,4 +1,4 @@
-import { Sequelize } from "sequelize";
+import { Op } from "sequelize";
 import { models } from "../models/models.js";
 
 class BearingController {
@@ -57,7 +57,7 @@ class BearingController {
 
       const existingCategory = await models.BearingDesign.findOne({
         where: {
-          [Sequelize.Op.or]: [{ name }, { title }, { url }],
+          [Op.or]: [{ name }, { title }, { url }],
         },
       });
 
@@ -124,12 +124,320 @@ class BearingController {
   }
   async getAll(req, res) {
     try {
-      const bearing = await models.Bearing.findAll();
+      let {
+        bearingDesignId,
+        bearingSealId,
+        bearingTypeId,
+        cornerId,
+        feature2Id,
+        featureId,
+        holeId,
+        innerRingId,
+        loadTypeId,
+        materialId,
+        outerRingId,
+        rollerTypeId,
+        rowCountId,
+        sealId,
+        sepId,
+        standartId,
+        bushingTypeId,
+        lubricationHoleId,
+        connectionId,
+        limit,
+        page,
+        minInnerDiameter,
+        maxInnerDiameter,
+        minOuterDiameter,
+        maxOuterDiameter,
+        minWidth,
+        maxWidth,
+        minPrice,
+        maxPrice,
+      } = req.body;
+
+      page = page || 1;
+      limit = limit || 12;
+
+      let offset = page * limit - limit;
+
+      const whereClause = {};
+
+      if (bearingDesignId) {
+        const bearingDesignIds = standartId.split("|");
+        whereClause.bearingDesignId = { [Op.in]: bearingDesignIds };
+      }
+
+      if (bearingSealId) {
+        const bearingSealIds = bearingSealId.split("|");
+        whereClause.bearingSealId = { [Op.in]: bearingSealIds };
+      }
+      if (bearingTypeId) {
+        const bearingTypeIds = bearingTypeId.split("|");
+        whereClause.bearingTypeId = { [Op.in]: bearingTypeIds };
+      }
+      if (cornerId) {
+        const cornerIds = cornerId.split("|");
+        whereClause.cornerId = { [Op.in]: cornerIds };
+      }
+      if (feature2Id) {
+        const feature2Ids = feature2Id.split("|");
+        whereClause.feature2Id = { [Op.in]: feature2Ids };
+      }
+      if (featureId) {
+        const feature2Ids = featureId.split("|");
+        whereClause.featureId = { [Op.in]: feature2Ids };
+      }
+      if (holeId) {
+        const holeIds = holeId.split("|");
+        whereClause.holeId = { [Op.in]: holeIds };
+      }
+      if (innerRingId) {
+        const innerRingIds = innerRingId.split("|");
+        whereClause.innerRingId = { [Op.in]: innerRingIds };
+      }
+      if (loadTypeId) {
+        const loadTypeIds = loadTypeId.split("|");
+        whereClause.loadTypeId = { [Op.in]: loadTypeIds };
+      }
+      if (materialId) {
+        const materialIds = materialId.split("|");
+        whereClause.materialId = { [Op.in]: materialIds };
+      }
+      if (outerRingId) {
+        const outerRingIds = materialId.split("|");
+        whereClause.outerRingId = { [Op.in]: outerRingIds };
+      }
+      if (rollerTypeId) {
+        const rollerTypeIds = rollerTypeId.split("|");
+        whereClause.rollerTypeId = { [Op.in]: rollerTypeIds };
+      }
+      if (rowCountId) {
+        const rowCountIds = rowCountId.split("|");
+        whereClause.rowCountId = { [Op.in]: rowCountIds };
+      }
+      if (sealId) {
+        const sealIds = sealId.split("|");
+        whereClause.sealId = { [Op.in]: sealIds };
+      }
+      if (sepId) {
+        const sepIds = sepId.split("|");
+        whereClause.sepId = { [Op.in]: sepIds };
+      }
+      if (bushingTypeId) {
+        const bushingTypeIds = bushingTypeId.split("|");
+        whereClause.bushingTypeId = { [Op.in]: bushingTypeIds };
+      }
+      if (lubricationHoleId) {
+        const lubricationHoleIds = lubricationHoleId.split("|");
+        whereClause.lubricationHoleId = { [Op.in]: lubricationHoleIds };
+      }
+      if (connectionId) {
+        const connectionIds = connectionId.split("|");
+        whereClause.connectionId = { [Op.in]: connectionIds };
+      }
+
+      const bearing = await models.Bearing.findAndCountAll({
+        where: whereClause,
+        limit,
+        offset,
+      });
       return res.json(bearing);
     } catch (error) {
       console.log(error);
+      return res.status(500).json({ message: "Ошибка при поиске подшипника" });
     }
   }
+
+  async getAllByQuery(req, res) {
+    try {
+      let {
+        bearingDesignId,
+        bearingSealId,
+        bearingTypeId,
+        cornerId,
+        feature2Id,
+        featureId,
+        holeId,
+        innerRingId,
+        loadTypeId,
+        materialId,
+        outerRingId,
+        rollerTypeId,
+        rowCountId,
+        sealId,
+        sepId,
+        standartId,
+        bushingTypeId,
+        lubricationHoleId,
+        connectionId,
+        limit,
+        page,
+        minInnerDiameter,
+        maxInnerDiameter,
+        minOuterDiameter,
+        maxOuterDiameter,
+        minWidth,
+        maxWidth,
+        minPrice,
+        maxPrice,
+        searchTerm,
+      } = req.query;
+
+      page = page || 1;
+      limit = limit || 12;
+
+      let offset = page * limit - limit;
+
+      const whereClause = {};
+
+      if (bearingDesignId) {
+        const bearingDesignIds = standartId.split("|");
+        whereClause.bearingDesignId = { [Op.in]: bearingDesignIds };
+      }
+
+      if (bearingSealId) {
+        const bearingSealIds = bearingSealId.split("|");
+        whereClause.bearingSealId = { [Op.in]: bearingSealIds };
+      }
+      if (bearingTypeId) {
+        const bearingTypeIds = bearingTypeId.split("|");
+        whereClause.bearingTypeId = { [Op.in]: bearingTypeIds };
+      }
+      if (cornerId) {
+        const cornerIds = cornerId.split("|");
+        whereClause.cornerId = { [Op.in]: cornerIds };
+      }
+      if (feature2Id) {
+        const feature2Ids = feature2Id.split("|");
+        whereClause.feature2Id = { [Op.in]: feature2Ids };
+      }
+      if (featureId) {
+        const feature2Ids = featureId.split("|");
+        whereClause.featureId = { [Op.in]: feature2Ids };
+      }
+      if (holeId) {
+        const holeIds = holeId.split("|");
+        whereClause.holeId = { [Op.in]: holeIds };
+      }
+      if (innerRingId) {
+        const innerRingIds = innerRingId.split("|");
+        whereClause.innerRingId = { [Op.in]: innerRingIds };
+      }
+      if (loadTypeId) {
+        const loadTypeIds = loadTypeId.split("|");
+        whereClause.loadTypeId = { [Op.in]: loadTypeIds };
+      }
+      if (materialId) {
+        const materialIds = materialId.split("|");
+        whereClause.materialId = { [Op.in]: materialIds };
+      }
+      if (outerRingId) {
+        const outerRingIds = materialId.split("|");
+        whereClause.outerRingId = { [Op.in]: outerRingIds };
+      }
+      if (rollerTypeId) {
+        const rollerTypeIds = rollerTypeId.split("|");
+        whereClause.rollerTypeId = { [Op.in]: rollerTypeIds };
+      }
+      if (rowCountId) {
+        const rowCountIds = rowCountId.split("|");
+        whereClause.rowCountId = { [Op.in]: rowCountIds };
+      }
+      if (sealId) {
+        const sealIds = sealId.split("|");
+        whereClause.sealId = { [Op.in]: sealIds };
+      }
+      if (sepId) {
+        const sepIds = sepId.split("|");
+        whereClause.sepId = { [Op.in]: sepIds };
+      }
+      if (bushingTypeId) {
+        const bushingTypeIds = bushingTypeId.split("|");
+        whereClause.bushingTypeId = { [Op.in]: bushingTypeIds };
+      }
+      if (lubricationHoleId) {
+        const lubricationHoleIds = lubricationHoleId.split("|");
+        whereClause.lubricationHoleId = { [Op.in]: lubricationHoleIds };
+      }
+      if (connectionId) {
+        const connectionIds = connectionId.split("|");
+        whereClause.connectionId = { [Op.in]: connectionIds };
+      }
+      if (searchTerm) {
+        whereClause[Op.or] = [
+          { title: { [Op.iLike]: `%${searchTerm}%` } },
+          { description: { [Op.iLike]: `%${searchTerm}%` } },
+          { content: { [Op.iLike]: `%${searchTerm}%` } },
+        ];
+      }
+
+      if (minInnerDiameter || maxInnerDiameter) {
+        whereClause.innerDiameter = {};
+
+        if (minInnerDiameter && !maxInnerDiameter) {
+          whereClause.innerDiameter[Op.gte] = Number(minInnerDiameter);
+        } else if (!minInnerDiameter && maxInnerDiameter) {
+          whereClause.innerDiameter[Op.lte] = Number(maxInnerDiameter);
+        } else if (minInnerDiameter && maxInnerDiameter) {
+          whereClause.innerDiameter[Op.between] = [
+            Number(minInnerDiameter),
+            Number(maxInnerDiameter),
+          ];
+        }
+      }
+
+      if (minOuterDiameter || maxOuterDiameter) {
+        whereClause.outerDiameter = {};
+        if (minOuterDiameter && !maxOuterDiameter) {
+          whereClause.outerDiameter[Op.gte] = Number(minOuterDiameter);
+        } else if (!minOuterDiameter && maxOuterDiameter) {
+          whereClause.outerDiameter[Op.lte] = Number(maxOuterDiameter);
+        } else if (minOuterDiameter && maxOuterDiameter) {
+          whereClause.outerDiameter[Op.between] = [
+            Number(minOuterDiameter),
+            Number(maxOuterDiameter),
+          ];
+        }
+      }
+
+      if (minWidth || maxWidth) {
+        whereClause.width = {};
+        if (minWidth && !maxWidth) {
+          whereClause.width[Op.gte] = Number(minWidth);
+        } else if (!minWidth && maxWidth) {
+          whereClause.width[Op.lte] = Number(maxWidth);
+        } else if (minWidth && maxWidth) {
+          whereClause.width[Op.between] = [Number(minWidth), Number(maxWidth)];
+        }
+      }
+
+      if (minPrice || maxPrice) {
+        whereClause.price = {};
+        // whereClause.priceRvz = {}
+
+        if (minPrice && !maxPrice) {
+          whereClause.price[Op.gte] = Number(minPrice);
+        } else if (!minPrice && maxPrice) {
+          whereClause.price[Op.lte] = Number(maxPrice);
+        } else if (minPrice && maxPrice) {
+          whereClause.price[Op.between] = [Number(minPrice), Number(maxPrice)];
+        }
+      }
+
+      const bearings = await models.Bearing.findAndCountAll({
+        where: whereClause,
+        limit,
+        offset,
+      });
+
+      return res.json(bearings);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Ошибка при поиске подшипника" });
+    }
+  }
+
   async getById(req, res) {}
 }
 
--- app/router/bearingRouter.js
+++ app/router/bearingRouter.js
@@ -2,8 +2,9 @@ import { Router } from "express";
 const router = new Router();
 import bearingController from "../controller/bearingController.js";
 
-router.post("/", bearingController.create);
-router.get("/", bearingController.getAll);
+router.post("/create", bearingController.create);
+router.post("/", bearingController.getAll);
+router.get("/", bearingController.getAllByQuery);
 router.get("/:id", bearingController.getById);
 
 export default router;
