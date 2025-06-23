4864817cbc688e8f07d76999b18ef8a8f6540323
Niakrist <ak_88dron@inbox.ru>
2025-06-23 12:29:30 +0300

    add proxi

--- index.js
+++ index.js
@@ -17,9 +17,19 @@ const app = express();
 
 app.use(cors());
 
+app.set("trust proxy", true);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, "app", "static")));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://rvzgroup.ru");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use("/api/v1", router);
 
 const start = async () => {
