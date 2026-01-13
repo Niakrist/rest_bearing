ef93c09b6bd7c3a135a5e08e51cb4dc8b9b9eea0
Niakrist <ak_88dron@inbox.ru>
2026-01-13 09:13:23 +0300

    fix: fix cors

--- index.js
+++ index.js
@@ -41,9 +41,15 @@ app.use((req, res, next) => {
 
   if (allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Credentials", "true");
   }
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept, Origin"); 
+  
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   next();
 });
 
